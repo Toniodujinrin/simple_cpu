578c4979250e6808ee66b19145c7ed5756b64025
Toni Odujinrin <todujinrin@gmail.com>
2025-11-08 19:33:33 -0600

    about done with base cache implementation

--- memory_modules/cache.v
+++ memory_modules/cache.v
@@ -271,113 +271,293 @@ endmodule
 
 
 
-module cache  #(parameter BLOCK_SIZE = 8, INDEX_WIDTH, TAG_WIDTH, DATA_WIDTH, BLOCK_OFFSET_WIDTH, WORD_OFFSET_WIDTH) (
+module cache  #(parameter BLOCK_SIZE = 8, INDEX_WIDTH = 8, TAG_WIDTH =4, WORD_WIDTH =16, WORD_OFFSET_WIDTH = 3) (
     input clk, reset, write_enabled, read_enabled, replace_request, 
     input [INDEX_WIDTH-1:0] set_index, 
     input [TAG_WIDTH-1:0] input_tag, 
-    input [DATA_WIDTH-1:0] write_data_word,
-    input [(DATA_WIDTH*BLOCK_SIZE)-1:0] write_data_block, 
-    input [WORD_OFFSET_WIDTH-1:0] write_byte_offset, read_byte_offset; 
-    output [DATA_WIDTH-1:0] read_data_word,
-    output [(DATA_WIDTH*BLOCK_SIZE)-1:0] read_data_block, evicted_data_block, 
-    output evict_flag, evict_dirty, evict_tag, data_ready
+    input [WORD_WIDTH-1:0] write_data_word,
+    input [(WORD_WIDTH*BLOCK_SIZE)-1:0] write_data_block, 
+    input [WORD_OFFSET_WIDTH-1:0] word_offset, //selects particular word in block 
+    output [WORD_WIDTH-1:0] read_data_word,
+    output [(WORD_WIDTH*BLOCK_SIZE)-1:0] read_data_block, evicted_data_block, 
+    output [TAG_WIDTH-1:0] evict_tag, 
+    output evict_flag, evict_dirty, data_ready, miss
 ); 
+    localparam DATA_WIDTH = WORD_WIDTH*BLOCK_SIZE; 
+    localparam SET_N = 2**INDEX_WIDTH;
+    localparam SET_WIDTH = 4; //number of ways in set
+
+    wire [(SET_N*DATA_WIDTH)-1:0] read_data_block_set; //data from all the blocks into one packed array 
+    wire [(SET_N*DATA_WIDTH)-1:0] evicted_data_block_set;
+    wire [(SET_N*TAG_WIDTH)-1:0] evicted_tag_set;
+    wire [SET_N-1:0] evict_flag_set; 
+    wire [SET_N-1:0] evict_dirty_set; 
+    wire [SET_N-1:0] miss_set; 
+    wire [SET_N-1:0] set_select; 
+
+    //output data muxes
+    reg_mux #(.REG_N(SET_N), .WIDTH(DATA_WIDTH))  READ_DATA_MUX(.in(read_data_block_set), .select(set_index), .out(read_data_block));
+    reg_mux #(.REG_N(SET_N), .WIDTH(DATA_WIDTH))  EVICTED_DATA_MUX(.in(evicted_data_block_set), .select(set_index), .out(evicted_data_block)); 
+    reg_mux #(.REG_N(SET_N), .WIDTH(TAG_WIDTH))  EVICTED_TAG_MUX(.in(evicted_tag_set), .select(set_index), .out(evict_tag)); 
+	register_address_decoder #(.INPUT_WIDTH(INDEX_WIDTH)) (.in(set_index),.out(set_select));  
     
     
-    
-
-
-endmodule
-
-
-module #(parameter SET_WIDTH, DATA_WIDTH, TAG_WIDTH) set(
-    input clk, reset, replace_request, write_enabled 
-    input tag_in 
-); 
-    localparam SET_DATA_LEN = SET_WIDTH * DATA_WIDTH;
-    localparam SET_TAG_LEN = SET_WIDTH * TAG_WIDTH; 
-    localparam PLRU_BIT_SIZE = 3; 
-
-
-    reg [SET_DATA_LEN-1:0] set_data; 
-    reg [SET_WIDTH-1:0] set_dirty_bits; //grouped dirty bits from all the ways 
-    reg [SET_WIDTH-1:0] set_valid_bits  //grouped valid bits from all the ways 
-    reg [SET_TAG_LEN-1:0] set_tag_bits; //grouped tag bits from all the ways
-
-
-    reg []
-    
-    wire [SET_WIDTH-1:0] set_select; //chooses the way whose tag matches the input tag 
-    wire tag_not_found; 
-
-    set_selector #(.SET_WIDTH(SET_WIDTH), .TAG_WIDTH(TAG_WIDTH), .SET_TAG_LEN(SET_TAG_LEN)) SELECTOR(
-        .tag_in(tag_in), 
-        .tags(set_tag_bits), 
-        .set_select(set_select), 
-        .tag_not_found(tag_not_found)
-    )
-
-    //plru update logic
-    always
-
-
-    generate
-        genvar i; 
-        for(i = 0; i < SET_WIDTH; i = i +1)
-            begin: way_instantiation 
-                way #(.DATA_WIDTH(DATA_WIDTH), .TAG_WIDTH(TAG_WIDTH)) WAY_N()
-            end 
-    endgenerate
-
-endmodule 
-
-module #(parameter SET_WIDTH, TAG_WIDTH, SET_TAG_LEN) set_selector( 
-    input [TAG_WIDTH-1:0] tag_in
-    input [SET_TAG_LEN-1:0] tags; 
-    output wire [SET_WIDTH-1:0] set_select //one hot ouput to select the actual way being refered to 
-    output reg tag_not_found; //asserted when the tag in 
-); 
+    //assignments
+    assign read_data_word = read_data_block[word_offset*WORD_WIDTH+:WORD_WIDTH]; 
+    assign miss = |miss_set; 
+    assign evict_flag = |evict_flag_set; 
+    assign evict_dirty = |evict_dirty_set; 
 
     generate 
         genvar i; 
-        for (i =0; i < SET_WIDTH; i = i +1)
+
+        for(i = 0; i < SET_N; i = i+1)
             begin 
-              assign set_select[i] =  tag_in == tags[TAG_WIDTH*i +: TAG_WIDTH];  //set_select[i] is set to 1 when the tag_in matches the i-th tag in the packed tags array 
-            end
+                set #(.SET_WIDTH(SET_WIDTH), .DATA_WIDTH(DATA_WIDTH), .TAG_WIDTH(TAG_WIDTH), .WORD_WIDTH(WORD_WIDTH), WORD_OFFSET_WIDTH(WORD_OFFSET_WIDTH)) SET(
+                    .clk(clk), 
+                    .reset(reset), 
+                    .replace_request(replace_request), 
+                    .write_enabled(write_enabled), 
+                    .set_selected(set_select[i]),
+                    .word_offset(word_offset), 
+                    .word_in(write_data_word),  
+                    .tag_in(input_tag), 
+                    .data_in(write_data_block), 
+                    .data(read_data_block_set[i*DATA_WIDTH+:DATA_WIDTH]), 
+                    .evict_data(evicted_data_block_set[i*DATA_WIDTH+:DATA_WIDTH]), 
+                    .evict_tag(evicted_tag_set[i*TAG_WIDTH+:TAG_WIDTH]), 
+                    .evict_flag(evict_flag_set[i]),
+                    .evict_dirty(evict_dirty_set[i]), 
+                    .miss(miss_set[i])
+                ); 
+            end 
     endgenerate 
 
-    assign tag_not_found = ~(|set_select); 
-
-endmodule 
+endmodule
 
 
-
-
-module #(parameter DATA_WIDTH, parameter TAG_WIDTH) way( 
-    input reset, clk, write_enabled, replace_request; 
-    output reg valid_bit, dirty_bit; 
-    input [DATA_WIDTH-1:0] data_in; 
-    input wire [TAG_WIDTH-1:0] tag_in
-    output reg [DATA_WIDTH-1:0] data_out; 
-    output reg [TAG_WIDTH-1:0] tag_out; 
+module set #(
+    parameter SET_WIDTH        = 4,
+    parameter DATA_WIDTH       = 128,
+    parameter TAG_WIDTH        = 4,
+    parameter WORD_WIDTH       = 16,
+    parameter WORD_OFFSET_WIDTH= 3
+) (
+    input  wire clk, reset, replace_request, write_enabled, set_selected,
+    input  wire [TAG_WIDTH-1:0] tag_in,
+    input  wire [DATA_WIDTH-1:0] data_in,     // block
+    input  wire [WORD_WIDTH-1:0] word_in,     // word
+    input  wire [WORD_OFFSET_WIDTH-1:0] word_offset,
+    output reg [DATA_WIDTH-1:0] data, evict_data,
+    output reg [TAG_WIDTH-1:0] evict_tag,
+    output reg evict_dirty,evict_flag,
+    output wire miss
 );
 
+    localparam LRU_COUNT_SIZE = $clog2(SET_WIDTH);
+
+    // per-way arrays 
+    wire [DATA_WIDTH-1:0]set_data      [0:SET_WIDTH-1];
+    wire set_valid_bits [0:SET_WIDTH-1];
+    wire set_evict_dirty_bits [0:SET_WIDTH-1];
+    wire set_evict_flag [0:SET_WIDTH-1];
+    wire [TAG_WIDTH-1:0] set_tag_bits   [0:SET_WIDTH-1];
+    wire [TAG_WIDTH-1:0] set_evict_tag  [0:SET_WIDTH-1];
+    wire [DATA_WIDTH-1:0] set_evict_data[0:SET_WIDTH-1];
+    wire [LRU_COUNT_SIZE-1:0] lru_count [0:SET_WIDTH-1];
+    wire [SET_WIDTH-1:0] way_select_one_hot;
+    reg  [LRU_COUNT_SIZE-1:0] lru_way;    
+    wire tag_not_found;
+
+
+
+    assign miss = set_selected & tag_not_found;
+
+
+    way_selector #(.SET_WIDTH(SET_WIDTH), .TAG_WIDTH(TAG_WIDTH)) selector_i (
+        .tag_in(tag_in),
+        .tags(set_tag_bits),
+        .valids(set_valid_bits),
+        .way_select(way_select_one_hot),
+        .tag_not_found(tag_not_found)
+    );
+
+    //lru update logic (specific to 4 ways) 
+    always @(*) 
+    begin 
+        if ((lru_count[0] < lru_count[1]) && 
+            (lru_count[0] < lru_count[2]) && 
+            (lru_count[0] < lru_count[3]) ) 
+            lru_way = 2'b00; 
+        else if( lru_count[1] < lru_count[2] && 
+                lru_count[1] < lru_count[3] ) 
+            lru_way = 2'b01; 
+        else if( lru_count[2] < lru_count[3] ) 
+            lru_way = 2'b10; 
+        else 
+            lru_way = 2'b11; 
+    end
+
+    // --- combinational muxes: select current data/evict_data/tag from the per-way arrays
+    always @(*) begin
+        data = {DATA_WIDTH{1'b0}};
+        evict_data = {DATA_WIDTH{1'b0}};
+        evict_tag  = {TAG_WIDTH{1'b0}};
+        evict_dirty = 1'b0;
+        evict_flag = 1'b0;
+        integer w; 
+        for (w = 0; w < SET_WIDTH; w = w + 1) begin
+            if (way_select_one_hot[w]) begin
+                data = set_data[w];
+            end
+            
+            if (w == lru_way) begin
+                evict_data  = set_evict_data[w];
+                evict_tag   = set_evict_tag[w];
+                evict_dirty = set_evict_dirty_bits[w];
+                evict_flag  = set_evict_flag[w];
+            end
+        end
+    end
+
+
+    genvar i;
+    generate
+        for (i = 0; i < SET_WIDTH; i = i + 1) begin : way_instantiation
+            way #(
+                .DATA_WIDTH(DATA_WIDTH),
+                .TAG_WIDTH(TAG_WIDTH),
+                .SET_WIDTH(SET_WIDTH),
+                .LRU_COUNT_SIZE(LRU_COUNT_SIZE)
+            ) WAY_N (
+                .clk(clk),
+                .reset(reset),
+                .write_enabled(write_enabled),
+                .replace_request(replace_request),
+                .way_selected( set_selected & ( replace_request ? (lru_way == i) : way_select_one_hot[i] ) ),
+                .data_in(data_in),
+                .word_in(word_in),
+                .word_offset(word_offset),
+                .tag_in(tag_in),
+                .valid_bit(set_valid_bits[i]),
+                .evict_dirty(set_evict_dirty_bits[i]),
+                .data(set_data[i]),
+                .evict_data(set_evict_data[i]),
+                .evict_flag(set_evict_flag[i]),
+                .tag(set_tag_bits[i]),
+                .evict_tag(set_evict_tag[i]),
+                .lru_count(lru_count[i])
+            );
+        end
+    endgenerate
+
+endmodule
+
+
+module way_selector #(
+    parameter SET_WIDTH = 4,
+    parameter TAG_WIDTH = 4
+) (
+    input  wire [TAG_WIDTH-1:0] tag_in,
+    input  wire [TAG_WIDTH-1:0] tags [0:SET_WIDTH-1],
+    input  wire valids [0:SET_WIDTH-1],
+    output wire [SET_WIDTH-1:0] way_select,    // one-hot
+    output wire tag_not_found
+);
+
+    genvar i;
+    generate
+        for (i = 0; i < SET_WIDTH; i = i + 1) 
+        begin : tag_match
+            assign way_select[i] = valids[i] & (tag_in == tags[i]);
+        end
+    endgenerate
+
+    assign tag_not_found = ~(|way_select);
+
+endmodule
+
+
+
+module way #(parameter DATA_WIDTH = 128, TAG_WIDTH = 4, SET_WIDTH = 4, LRU_COUNT_SIZE = 2, WORD_OFFSET_WIDTH = 3, WORD_WIDTH = 16) ( 
+    input reset, clk, write_enabled, replace_request, way_selected, 
+    output reg valid_bit, evict_dirty, evict_flag, 
+    input [DATA_WIDTH-1:0] data_in,
+    input [WORD_WIDTH-1:0] word_in, 
+    input [WORD_OFFSET_WIDTH-1:0] word_offset,
+    input wire [TAG_WIDTH-1:0] tag_in, 
+    output reg [DATA_WIDTH-1:0] data, evict_data,
+    output reg [TAG_WIDTH-1:0] tag, evict_tag,
+    output wire [LRU_COUNT_SIZE-1:0] lru_count
+); 
+    
+    reg dirty_bit; 
+    //Counter
+    lru_counter #(.COUNTER_WIDTH(LRU_COUNT_SIZE)) LRU(
+        .clk(clk), 
+        .reset(reset), 
+        .enabled(way_selected), 
+        .sync_reset(replace_request & way_selected),
+        .count(lru_count)
+    ); 
+    //IO policy 
+    always @(posedge clk or posedge reset) begin
+        if (reset) 
+            begin
+                tag       <= {TAG_WIDTH{1'b0}};
+                data      <= {DATA_WIDTH{1'b0}};
+                valid_bit <= 1'b0;
+                dirty_bit <= 1'b0;
+                evict_flag<= 1'b0;
+                evict_dirty<= 1'b0;
+                evict_data <= {DATA_WIDTH{1'b0}};
+                evict_tag  <= {TAG_WIDTH{1'b0}};
+            end 
+        else 
+            begin
+                evict_flag <= 1'b0;
+                evict_dirty <= 1'b0;
+                evict_data <= evict_data; 
+                evict_tag <= evict_tag;
+
+        
+                if (replace_request && way_selected) 
+                    begin
+                        //evict old 
+                        evict_data  <= data;
+                        evict_tag   <= tag;
+                        evict_dirty <= dirty_bit;
+                        // install new block
+                        data        <= data_in;
+                        tag         <= tag_in;
+                        valid_bit   <= 1'b1;
+                        dirty_bit   <= 1'b0;
+                        // evict flag indicates we have evicted a valid line
+                        evict_flag  <= valid_bit ? 1'b1 : 1'b0;
+                    end 
+                else if (write_enabled && way_selected) 
+                    begin
+                        // Write to specific word within block
+                        data[word_offset*WORD_WIDTH +: WORD_WIDTH] <= word_in;
+                        dirty_bit <= 1'b1;
+                    end
+        end
+    end
+endmodule
+
+
+module lru_counter #(parameter COUNTER_WIDTH = 2)(
+    input clk, reset, enabled, sync_reset, 
+    output reg [COUNTER_WIDTH-1:0] count
+)
     always @(posedge clk, posedge reset)
     begin 
-        if(reset) 
-            tag_out <= 0; 
-            data_out <= 0; 
-            valid_bit <= 0; 
-            dirty_bit <= 0; 
-        else if (write_enabled)
-            data_out <= data_in;
-            dirty_bit <= 1; 
-        else if (replace_request)
-            data_out <= data_in; 
-            tag_out <= tag_in;
-            valid_bit <= 1;
-            dirty_bit <= 0;    
-        
+        if(reset)
+            count <= {COUNTER_WIDTH{1'b0}};
+        else if (sync_reset)
+            count <= {COUNTER_WIDTH{1'b0}};
+        else if (enabled)
+            count <= count+1;  
     end 
-    
-endmodule
+
+endmodule 
